3d64dc81a682690f2a30841b3ab62ddfcb9556cf
mengpeng <pengmeng@yangqianguan.com>
2021-05-21 19:41:50 +0800

    [blog][ci] 修正部署脚本

--- scripts/getscope.js
+++ scripts/getscope.js
@@ -1,6 +1,6 @@
 const {execSync} = require('child_process');
 
-// const core = require('@actions/core');
+const core = require('@actions/core');
 
 const shell = require('shelljs');
 const chalk = require('chalk');
@@ -11,7 +11,7 @@ const {green, cyan} = chalk;
 let msg = execSync('git --no-pager log -1 --pretty=format:"%s"').toString();
 const scope = msg.match(/\[(\w+.\w+)\]/)[1];
 log(green(`开始部署子项目`,cyan(` ${scope} `)));
-// core.exportVariable('SCOPE', scope);
+core.exportVariable('SCOPE', scope);
 shell.cd(`projects/${scope}`)
 log(green(`开始安装依赖并 build`));
 shell.exec('npm run ali:build')
