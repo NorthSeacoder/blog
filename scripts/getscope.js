0f70c65aeab6ac455979958150729dffcc3837a7
mengpeng <pengmeng@yangqianguan.com>
2021-05-21 19:01:11 +0800

    [blog][init] 测试新部署脚本

--- scripts/getscope.js
+++ scripts/getscope.js
@@ -1,7 +1,17 @@
 const {execSync} = require('child_process');
-const core = require('@actions/core');
+
+// const core = require('@actions/core');
+
+const shell = require('shelljs');
+const chalk = require('chalk');
+
+const {log} = console;
+const {green, cyan} = chalk;
 
 let msg = execSync('git --no-pager log -1 --pretty=format:"%s"').toString();
 const scope = msg.match(/\[(\w+.\w+)\]/)[1];
-
-core.exportVariable('SCOPE', scope);
+log(green(`开始部署子项目`,cyan(` ${scope} `)));
+// core.exportVariable('SCOPE', scope);
+shell.cd(`projects/${scope}`)
+log(green(`开始安装依赖并 build`));
+shell.exec('npm run ali:build')
